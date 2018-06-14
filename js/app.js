80aa40647070d7a0826901a64999537c04c94dac
Laurie Carano <lauriecarano@gmail.com>
2018-06-13 22:16:53 -0600

    Commit 4 - Fizz Buzz answered

--- js/app.js
+++ js/app.js
@@ -19,11 +19,24 @@
 
 // Commit 3 - Excited Kittens answered
 
-let excitedKitten = ["...human...why you taking pictures of me?...", "...the catnip made me do it...", "...why does the red dot always get away..."];
+// let excitedKitten = ["...human...why you taking pictures of me?...", "...the catnip made me do it...", "...why does the red dot always get away..."];
 
-for (let i = 1; i <= 20; i++) {
-	if (i % 2 ==0) {
-		console.log(excitedKitten[Math.floor(Math.random() * 3)]);
-	}
+// for (let i = 1; i <= 20; i++) {
+// 	if (i % 2 ==0) {
+// 		console.log(excitedKitten[Math.floor(Math.random() * 3)]);
+// 	}
+// }
+
+// Commit 4 - Fizz Buzz answered
+
+for (let i = 1; i <= 100; i++) {
+ if (i % 5 === 0 && i % 3 === 0) {
+ 	console.log("FizzBuzz");
+ } else if (i % 3 === 0) {
+ 	console.log("Fizz");
+ } else if (i % 5 === 0) {
+ 	console.log("Buzz");
+ } else {
+ 	console.log(i);
+ }
 }
-
