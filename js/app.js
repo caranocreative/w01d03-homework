526fb443774519bd0ded7a07d6b880be7482df58
Laurie Carano <lauriecarano@gmail.com>
2018-06-13 21:15:59 -0600

    Commit 2 - Get Even answered

--- js/app.js
+++ js/app.js
@@ -3,6 +3,15 @@
 
 // Commit 1 - Easy Going answered
 
-for (let i = 0; i <= 20; i++) {
+// for (let i = 0; i <= 20; i++) {
+// 	console.log(i);
+// }
+
+
+// Commit 2 - Get Even answered
+
+for (let i = 2; i < 200; i++) {
+	if (i % 2 == 0);
 	console.log(i);
+	i++
 }
