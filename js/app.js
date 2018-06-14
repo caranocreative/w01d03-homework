a497232aaa3588e0e77c34740b6a0c9ec7fb5a4d
Laurie Carano <lauriecarano@gmail.com>
2018-06-13 21:40:29 -0600

    Commit 2 - Get Even answered - fixed loop

--- js/app.js
+++ js/app.js
@@ -11,7 +11,17 @@
 // Commit 2 - Get Even answered
 
 for (let i = 2; i < 200; i++) {
-	if (i % 2 == 0);
-	console.log(i);
-	i++
+	if (i % 2 == 0) {
+		console.log(i);
+	}
+	//i++
 }
+
+
+// Commit 3 - Excited Kittens answered
+
+// for (let i = 1; i <= 20; i++) {
+// 	if (i % 2 ==0);
+// 	console.log("Love me, pet me! HSSSSSS");
+// }
+
